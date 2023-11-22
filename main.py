f19db8ed78ca7f509d03cb958f9f07b43900a884
unusdv <asilbekdilboboev77@gmail.com>
2023-11-22 00:40:47 -1100

    python calculator

---
+++ main.py
@@ -0,0 +1,49 @@
+# Calculator program
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero"
+
+# Main program
+while True:
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter choice (1-5): ")
+
+    if choice == '5':
+        print("Exiting calculator...")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result:", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid input. Please try again.")
